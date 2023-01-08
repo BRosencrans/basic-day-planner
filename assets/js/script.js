dd17ebbd77dd700a19c6f113b2ff7ebe3e3603ad
BRosencrans <brandon.rosencrans@gmail.com>
2023-01-07 20:02:39 -0800

    fixed id's for javascript variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,42 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+//time card button elements
+var sevenAmBtn= ("#seven-am-save-to-page");
+var eightAmBtn= $("#eight-am-save-to-page")
+var nineAmBtn= $("#nine-am-save-to-page")
+var tenAmBtn= $("#ten-am-save-to-page")
+var elevenAmBtn= $("#eleven-am-save-to-page")
+var twelvePmBtn= $("#twelve-pm-save-to-page")
+var onePmBtn= $("#one-pm-save-to-page")
+var twoPmBtn= $("#two-pm-save-to-page")
+var threePmBtn= $("#three-pm-save-to-page")
+var fourPmBtn= $("#four-pm-save-to-page")
+var fivePmBtn= $("#five-pm-save-to-page")
+var sixPmBtn= $("#six-pm-save-to-page")
+var sevenPmBtn= $("#seven-pm-save-to-page")
+//grabs all the textarea tags and puts them in an array
+var timeArray = $("textarea").get()
+console.log(timeArray)
+//Turns all textareas in to jquery objects
+var textAreaSevenAm = $(timeArray[0])
+var textAreaEightAm = $(timeArray[1])
+var textAreaNineAm = $(timeArray[2])
+var textAreaTenAm = $(timeArray[3])
+var textAreaElevenAm = $(timeArray[4])
+var textAreaTwelevePm = $(timeArray[5])
+var textAreaOnePm = $(timeArray[6])
+var textAreaTwoPm = $(timeArray[7])
+var textAreaThreePm = $(timeArray[8])
+var textAreaFourPm = $(timeArray[9])
+var textAreaFivePm = $(timeArray[10])
+var textAreaSixPm = $(timeArray[11])
+var textAreaSevenPm = $(timeArray[12])
+
+
+
+
+//$(function  () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +55,4 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+//});
